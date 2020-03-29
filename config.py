f3eb6769e69d9803762ba1317dde15827e856c99
shingo <fromjapan64@gmail.com>
2020-03-29 21:52:53 +0900

    chromeのみのショートカットをchromeだけになるように変更

--- config.py
+++ config.py
@@ -23,16 +23,18 @@ def configure(keymap):
 #仮想ウデスクトップ移動
         keymap_global[ "LC-LW-LA-Left" ] = "LC-LW-Right"
         keymap_global[ "LC-LW-LA-Right" ] = "LC-LW-Left"
-#クローム用タブ切り替え
-        keymap_global[ "RS-LS-Z" ] = "C-1"
-        keymap_global[ "RS-LS-X" ] = "C-2"
-        keymap_global[ "RS-LS-C" ] = "C-3"
-        keymap_global[ "RS-LS-V" ] = "C-4"
-        keymap_global[ "RS-LS-B" ] = "C-5"
-#クローム用タブ削除
-        keymap_global[ "LC-Delete" ] = "LC-W"
 # アンダースコア入れ替え
         keymap_global[ "Underscore" ] = "S-Underscore"
         keymap_global[ "S-Underscore" ] = "Underscore"
 #無変換をWinキーに置き換え
         keymap.replaceKey( 29, "LWin" )
+
+        keymap_chrome = keymap.defineWindowKeymap( exe_name="chrome.exe" )
+#クローム用タブ切り替え
+        keymap_chrome[ "RS-LS-Z" ] = "C-1"
+        keymap_chrome[ "RS-LS-X" ] = "C-2"
+        keymap_chrome[ "RS-LS-C" ] = "C-3"
+        keymap_chrome[ "RS-LS-V" ] = "C-4"
+        keymap_chrome[ "RS-LS-B" ] = "C-5"
+#クローム用タブ削除
+        keymap_chrome[ "LC-Delete" ] = "LC-W"
